0cc978cff7e79bbff81584e3561dfeafaf351283
Eduard <edik200824@gmail.com>
2024-08-06 21:17:29 +0300

    some fixes

--- src/main/java/frc/robot/commands/ArmDownCommand.java
+++ src/main/java/frc/robot/commands/ArmDownCommand.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSystem;
 
 public class ArmDownCommand extends Command {
-    private ArmSystem armSystem;
+    private final ArmSystem armSystem;
 
     public ArmDownCommand(ArmSystem armSystem) {
         this.armSystem = armSystem;
