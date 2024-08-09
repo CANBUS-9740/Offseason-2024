2567f28d5d6cbef781273b7a69bc689c46c5907e
Eduard <edik200824@gmail.com>
2024-08-09 22:22:29 +0300

    some fixes 3, phase 1

--- src/main/java/frc/robot/subsystems/ArmSystem.java
+++ src/main/java/frc/robot/subsystems/ArmSystem.java
@@ -9,16 +9,16 @@ import frc.robot.RobotMap;
 public class ArmSystem extends SubsystemBase {
     private final CANSparkMax motor;
     private final RelativeEncoder neoEncoder;
-    private final DutyCycleEncoder dutyCycleEncoder;
+    private final DutyCycleEncoder absEncoder;
 
     public ArmSystem() {
         motor = new CANSparkMax(RobotMap.ARM_MOTOR_PORT, CANSparkLowLevel.MotorType.kBrushless);
         neoEncoder = motor.getEncoder();
-        dutyCycleEncoder = new DutyCycleEncoder(RobotMap.ARM_ENCODER_PORT);
+        absEncoder = new DutyCycleEncoder(RobotMap.ARM_ENCODER_PORT);
 
         motor.restoreFactoryDefaults();
 
-        neoEncoder.setPosition(dutyCycleEncoder.getAbsolutePosition() - RobotMap.ABSOLUTE_ENCODER_ZERO_OFFSET);
+        neoEncoder.setPosition((absEncoder.getAbsolutePosition() - RobotMap.ABSOLUTE_ENCODER_ZERO_OFFSET) / RobotMap.ARM_GEAR_RATIO);
         motor.setSoftLimit(CANSparkBase.SoftLimitDirection.kForward, (float) ((RobotMap.ARM_MAX_ANGLE * RobotMap.ARM_GEAR_RATIO) / 360));
         motor.enableSoftLimit(CANSparkBase.SoftLimitDirection.kForward, true);
         motor.setSoftLimit(CANSparkBase.SoftLimitDirection.kReverse, (float) ((RobotMap.ARM_MIN_ANGLE * RobotMap.ARM_GEAR_RATIO) / 360));
@@ -49,7 +49,7 @@ public class ArmSystem extends SubsystemBase {
             // ↓ For offset the encoder to zero point that we want(floor),
             // ↓ we subtract from the encoder position the required quantity that need to offset the encoder to zero.
             // ↓ The "-" at the start we add to turn over the encoder value to positive value
-        return -(dutyCycleEncoder.getAbsolutePosition() - RobotMap.ABSOLUTE_ENCODER_ZERO_OFFSET) * 360;
+        return -(absEncoder.getAbsolutePosition() - RobotMap.ABSOLUTE_ENCODER_ZERO_OFFSET) * 360;
     }
 
     @Override
