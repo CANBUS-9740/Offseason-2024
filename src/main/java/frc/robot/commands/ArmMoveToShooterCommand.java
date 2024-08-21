9d65c283236a9651eb5ee6acc7e94445c3a193ec
Eduard <edik200824@gmail.com>
2024-08-21 22:48:45 +0300

    Added phase 2 without neo encoder

--- src/main/java/frc/robot/commands/ArmMoveToShooterCommand.java
+++ src/main/java/frc/robot/commands/ArmMoveToShooterCommand.java
@@ -1,25 +1,31 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.ArmSystem;
 
 public class ArmMoveToShooterCommand extends Command {
     private final ArmSystem armSystem;
+    private final PIDController pidController;
 
     public ArmMoveToShooterCommand(ArmSystem armSystem) {
         this.armSystem = armSystem;
+        pidController = new PIDController(0.07, 0, 0);
 
         addRequirements(armSystem);
     }
 
     @Override
     public void initialize() {
-        armSystem.moveToPosition(RobotMap.ARM_SHOOTER_ANGLE);
+        pidController.reset();
     }
 
     @Override
     public void execute() {
+        double output = pidController.calculate(armSystem.getAbsEncoderPositionDegrees(),  RobotMap.ARM_SHOOTER_ANGLE);
+
+        armSystem.moveToAngle(output);
     }
 
     @Override
